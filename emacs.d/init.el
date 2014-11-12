c4a2f840b2ff3361d521b4353838318cc57e0554
Maximilian Gerlach <m@px0.de>
2014-11-12 07:35:57 -0500

    remove wrong evil

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,9 @@
 (require 'package)
 
 
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
+; (add-to-list 'load-path "~/.emacs.d/evil")
+; (require 'evil)
+; (evil-mode 1)
 
 
 (add-to-list 'package-archives
