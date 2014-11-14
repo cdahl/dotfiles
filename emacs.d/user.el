55fa7ca9e68a7e29a10f3462399b22aebfde2272
Max Gerlach <mgerlach@klick.com>
2014-11-14 14:24:05 -0500

    ido kill buffer: ,k

--- emacs.d/user.el
+++ emacs.d/user.el
@@ -127,9 +127,10 @@ Position the cursor at it's beginning, according to the current mode."
   "C-T" 'textmate-clear-cache
   "u" 'undo-tree-visualize
   "." 'evil-search-highlight-persist-remove-all
-  "x" 'helm-M-x
+  "x" 'smex
   "h" 'dired-jump
   "v" 'split-window-right
+  "k" 'ido-kill-buffer
   "," 'other-window
   "s" 'pp-eval-last-sexp
   "w" 'save-buffer
